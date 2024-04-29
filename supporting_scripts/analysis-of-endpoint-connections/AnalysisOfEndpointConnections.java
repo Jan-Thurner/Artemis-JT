b120e961851d566046d344b236a138d1c8bc6441
Jan-Thurner <107639007+Jan-Thurner@users.noreply.github.com>
2024-04-29 14:02:07 +0200

    extract java file to supporting scripts

--- supporting_scripts/analysis-of-endpoint-connections/AnalysisOfEndpointConnections.java
+++ supporting_scripts/analysis-of-endpoint-connections/AnalysisOfEndpointConnections.java
@@ -1,6 +1,7 @@
 package analysisOfEndpointConnections;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.Collection;
 
 import com.thoughtworks.qdox.JavaProjectBuilder;
@@ -16,8 +17,19 @@ public class AnalysisOfEndpointConnections {
      * @param args Unused.
      */
     public static void main(String[] args) {
+        String[] testArray = new String[2];
+        testArray[0] = "src/main/java/de/tum/in/www1/artemis/web/rest/tutorialgroups/TutorialGroupFreePeriodResource.java";
+        testArray[1] = "src/main/webapp/app/course/tutorial-groups/services/tutorial-group-free-period.service.ts";
+
+        String[] serverFiles = Arrays.stream(testArray).filter(filePath -> filePath.endsWith(".java")).toArray(String[]::new);
+
+        analyzeServerEndpoints(serverFiles);
+        analyzeClientRESTCalls(testArray);
+    }
+
+    private static void analyzeServerEndpoints(String[] filePaths) {
         JavaProjectBuilder builder = new JavaProjectBuilder();
-        for (String filePath : args) {
+        for (String filePath : filePaths) {
             builder.addSourceTree(new File(filePath));
         }
 
@@ -28,11 +40,17 @@ public class AnalysisOfEndpointConnections {
                     if (annotation.getType().getFullyQualifiedName().startsWith("org.springframework.web.bind.annotation")) {
                         System.out.println("Endpoint: " + method.getName());
                         System.out.println("HTTP Method: " + annotation.getType().getName());
+                        System.out.println("Path: " + annotation.getProperty("value"));
                         System.out.println("Class: " + javaClass.getFullyQualifiedName());
+                        System.out.println("Line: " + method.getLineNumber());
                         System.out.println("---------------------------------------------------");
                     }
                 }
             }
         }
+    }
+
+    private static void analyzeClientRESTCalls (String[] filePaths) {
+
     }
 }
