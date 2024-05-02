08130e256870470eb89e6364719e32c206e83608
Jan-Thurner <107639007+Jan-Thurner@users.noreply.github.com>
2024-05-02 14:06:15 +0200

    fixed compilation error

--- supporting_scripts/analysis-of-endpoint-connections/src/main/java/AnalysisOfEndpointConnections.java
+++ supporting_scripts/analysis-of-endpoint-connections/src/main/java/AnalysisOfEndpointConnections.java
@@ -18,14 +18,8 @@ public class AnalysisOfEndpointConnections {
      * @param args Unused.
      */
     public static void main(String[] args) {
-        String[] testArray = new String[2];
-        testArray[0] = "src/main/java/de/tum/in/www1/artemis/web/rest/tutorialgroups/TutorialGroupFreePeriodResource.java";
-        testArray[1] = "src/main/webapp/app/course/tutorial-groups/services/tutorial-group-free-period.service.ts";
-
-        String[] serverFiles = Arrays.stream(testArray).filter(filePath -> filePath.endsWith(".java")).toArray(String[]::new);
-
+        String[] serverFiles = Arrays.stream(args).filter(filePath -> filePath.endsWith(".java")).toArray(String[]::new);
         analyzeServerEndpoints(serverFiles);
-        analyzeClientRESTCalls(testArray);
     }
 
     private static void analyzeServerEndpoints(String[] filePaths) {
@@ -37,14 +31,16 @@ public class AnalysisOfEndpointConnections {
         Collection<JavaClass> classes = builder.getClasses();
         for (JavaClass javaClass : classes) {
             Optional<JavaAnnotation> requestMappingOptional = javaClass.getAnnotations().stream()
-                .filter(annotaion ->
-                    annotaion.getType().getFullyQualifiedName().startsWith("org.springframework.web.bind.annotation")
-                    && annotaion.getProperty("value").equals("RequestMapping"))
+                .filter(annotation ->
+                    annotation.getType().getFullyQualifiedName().startsWith("org.springframework.web.bind.annotation")
+                    && annotation.getProperty("value").equals("RequestMapping"))
                 .findFirst();
             for (JavaMethod method : javaClass.getMethods()) {
                 for (JavaAnnotation annotation : method.getAnnotations()) {
                     if (annotation.getType().getFullyQualifiedName().startsWith("org.springframework.web.bind.annotation")) {
-                        requestMappingOptional.isPresent()? System.out.println("Request Mapping: " + requestMappingOptional.get().getProperty("value"))
+                        if (requestMappingOptional.isPresent()) {
+                            System.out.println("Request Mapping: " + requestMappingOptional.get().getProperty("value"));
+                        };
                         System.out.println("Endpoint: " + method.getName());
                         System.out.println("HTTP Method: " + annotation.getType().getName());
                         System.out.println("Path: " + annotation.getProperty("value"));
@@ -55,9 +51,5 @@ public class AnalysisOfEndpointConnections {
                 }
             }
         }
-    }
-
-    private static void analyzeClientRESTCalls(String[] filePaths) {
-
     }
 }
